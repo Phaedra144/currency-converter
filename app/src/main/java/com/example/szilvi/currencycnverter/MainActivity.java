b5c282d089410a3fe4d16e9a3fc9f251ef24da03
Phaedra144 <bedi.szilvia@gmail.com>
2019-01-04 16:18:20 +0100

    fix(MainActivity): tag data type bugs

--- app/src/main/java/com/example/szilvi/currencycnverter/MainActivity.java
+++ app/src/main/java/com/example/szilvi/currencycnverter/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         currencyButt = findViewById(R.id.currencyButton);
+        currencyButt.setTag(1);
     }
 
     public void convert(View view) {
