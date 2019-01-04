136d1a92a742daa70e445246adb6d4984ab6dafa
Phaedra144 <bedi.szilvia@gmail.com>
2019-01-04 15:57:36 +0100

    feat(MainActivity): convert method

--- app/src/main/java/com/example/szilvi/currencycnverter/MainActivity.java
+++ app/src/main/java/com/example/szilvi/currencycnverter/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.szilvi.currencycnverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convert(View view) {
+        EditText amount = findViewById(R.id.amountEditText);
+        Button currencyButt = findViewById(R.id.currencyButton);
+        Double amountInDoub = Double.parseDouble(amount.getText().toString());
+        String result = "";
+        if (currencyButt.getText().equals("EUR to HUF")) {
+            amountInDoub *= 314;
+            result = amountInDoub + " HUF";
+        } else {
+            amountInDoub /= 314;
+            result = amountInDoub + " EUR";
+        }
+        Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT);
     }
 }
